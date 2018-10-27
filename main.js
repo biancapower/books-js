e2dba2fee99f318a4b826f8f8e162abaeabcbd5c
Bianca Power <drbiancampower@gmail.com>
2018-10-27 22:07:23 +1100

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let isbn = prompt("Please enter ISBN", "0449212602");
+
+$.getJSON(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=data&format=json`, function(data) {
+    //JSON is in "data"
+  $("#title").append(data[`ISBN:${isbn}`].title);
+  $("#numPages").append(data[`ISBN:${isbn}`].number_of_pages);
+  $("#coverImage").attr("src", data[`ISBN:${isbn}`].cover.large);
+});
